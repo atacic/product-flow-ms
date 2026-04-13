1364147b8aaad6999a6c7f0024b468f7b72a9e1f
aleksa <tacicaleksaa@gmail.com>
2026-04-13 22:37:29 +0200

    - inventory-service: circuit breaker dependencies and impl logic

--- inventory-service/src/main/java/com/aleksa/inventory_service/service/InventoryService.java
+++ inventory-service/src/main/java/com/aleksa/inventory_service/service/InventoryService.java
@@ -2,17 +2,22 @@ package com.aleksa.inventory_service.service;
 
 import com.aleksa.inventory_service.client.CatalogFeignClient;
 import com.aleksa.inventory_service.model.AvailabilityResponse;
-import jakarta.annotation.PostConstruct;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
 
 @Service
 public class InventoryService {
 
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
     private final CatalogFeignClient catalogFeignClient;
     private final Map<Integer, Boolean> inventory = new ConcurrentHashMap<>();
     private final Random random = new Random();
@@ -21,9 +26,23 @@ public class InventoryService {
         this.catalogFeignClient = catalogFeignClient;
     }
 
-    @PostConstruct
-    void initialize() {
+    @Retry(name = "inventory-retry")
+    @CircuitBreaker(name = "inventory-service-cb", fallbackMethod = "initializeFallback")
+    @EventListener(ApplicationReadyEvent.class)
+    public void initialize() {
+
+        logger.info("Inventory initializing...");
+
+        if (!inventory.isEmpty()) {
+            return;
+        }
         catalogFeignClient.getAllProducts().forEach(p -> inventory.put(p.productId(), random.nextBoolean()));
+
+        logger.info("Inventory initialized with " + inventory.size() + " products");
+    }
+
+    public void initializeFallback(Throwable t) {
+        logger.warning("Circuit breaker fallback triggered for inventory initialization. Service unavailable: " + t.getMessage());
     }
 
     public Optional<AvailabilityResponse> getAvailability(int productId) {
